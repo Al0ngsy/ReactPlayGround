b9dbe5fe79413fe27fcd9b798b0288f039e7244f
Le Quoc Anh Tran <tranlequocanh@ymail.com>
2019-09-15 15:05:34 +0200

    correcting typo

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const NotFound = () => <h1 style={{textAlign: "center"}}>Welcome To My React Playground</h1>
+const Main = () => <h1 style={{textAlign: "center"}}>Welcome To My React Playground</h1>
 
-export default NotFound 
+export default Main 
